5f95ede87ea86cc83a97dbd495a0703d52b464f0
Florian Tournay <florian.tournay@hotmail.fr>
2024-10-17 11:36:57 +0200

    Add snap button event binding

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -13,6 +13,8 @@ export class FaceSnapComponent implements OnInit {
   createdAt!: Date;
   snaps!: number;
   imageUrl!: string;
+  userHasSnapped!: boolean;
+  snapButtonText!: string;
 
   ngOnInit(): void {
     this.title = "La Tour Eiffel";
@@ -20,5 +22,27 @@ export class FaceSnapComponent implements OnInit {
     this.createdAt = new Date();
     this.snaps = 5;
     this.imageUrl = "https://cdn.pixabay.com/photo/2015/10/06/18/26/eiffel-tower-975004_1280.jpg";
+    this.userHasSnapped = false;
+    this.snapButtonText = "snap !";
+  }
+
+  onSnap(){
+    if(this.userHasSnapped){
+      this.unSnap();
+    } else{
+      this.snap();
+    }
+  }
+
+  unSnap(){
+    this.snapButtonText = "snap !";
+    this.snaps--;
+    this.userHasSnapped = false;
+  }
+
+  snap(){
+    this.snapButtonText = "unsnap !";
+    this.snaps++;
+    this.userHasSnapped = true;
   }
 }
