3f041f4c90bb6a75021b9bd07eeb33c832bfed08
Florian Tournay <florian.tournay@hotmail.fr>
2024-10-18 12:12:21 +0200

    Display items facesnap list with @for control flow block

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,33 +12,39 @@ import { FaceSnap } from './models/face-snap';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
+
+  faceSnaps!: FaceSnap[];
+
   mySnap!: FaceSnap;
   myOtherSnap!: FaceSnap;
   myLastSnap!: FaceSnap;
 
   ngOnInit(): void {
-    this.mySnap = new FaceSnap(
-      "La Tour Eiffel",
-      "Ceci est une photo prise devant la tour Eiffel",
-      "https://cdn.pixabay.com/photo/2015/10/06/18/26/eiffel-tower-975004_1280.jpg",
-      new Date(),
-      5
-    );
-    this.myOtherSnap = new FaceSnap(
-      "L'Arc de Triomphe",
-      "Photo des Champs Elysées donnant lieu sur l'Arc de Triomphe de Paris",
-      "https://cdn.pixabay.com/photo/2013/04/07/21/29/arc-de-triomphe-101633_1280.jpg",
-      new Date(),
-      10
-    );
-    this.myLastSnap = new FaceSnap(
-      "Le Louvre",
-      "Photo de la pyramide du Louvre",
-      "https://cdn.pixabay.com/photo/2020/11/22/19/19/louvre-5767708_1280.jpg",
-      new Date(),
-      25
-    );
+    this.faceSnaps = [
+      new FaceSnap(
+        "La Tour Eiffel",
+        "Ceci est une photo prise devant la tour Eiffel",
+        "https://cdn.pixabay.com/photo/2015/10/06/18/26/eiffel-tower-975004_1280.jpg",
+        new Date(),
+        5
+      ),
+      new FaceSnap(
+        "L'Arc de Triomphe",
+        "Photo des Champs Elysées donnant lieu sur l'Arc de Triomphe de Paris",
+        "https://cdn.pixabay.com/photo/2013/04/07/21/29/arc-de-triomphe-101633_1280.jpg",
+        new Date(),
+        10
+      ),
+      new FaceSnap(
+        "Le Louvre",
+        "Photo de la pyramide du Louvre",
+        "https://cdn.pixabay.com/photo/2020/11/22/19/19/louvre-5767708_1280.jpg",
+        new Date(),
+        25
+      ),
+    ];
 
-    this.myOtherSnap.setLocation('Champs Elysées');
+
+    this.faceSnaps[1].setLocation('Champs Elysées');
   }
 }
