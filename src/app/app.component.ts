9862669fbadf2e733cd34604eca378e632e66b61
Florian Tournay <florian.tournay@hotmail.fr>
2024-12-21 11:46:47 +0100

    Add tap side effet level observable operators on observable pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
-import { filter, interval, map, Observable } from 'rxjs';
+import { filter, interval, map, Observable, tap } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 
 @Component({
@@ -25,7 +25,12 @@ export class AppComponent implements OnInit{
       map(value => value % 2 === 0 ?
         `Je suis ${value} et je suis pair`:
         `Je suis ${value} et je suis impair`
-      )
+      ),
+      tap(text => this.logger(text))
     );
+  }
+
+  logger(text: string) {
+    console.log(`Log: ${text}`);
   }
 }
