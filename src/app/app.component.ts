0b71b54642f0b1e5c17838cec3509bab77119e3b
Florian Tournay <florian.tournay@hotmail.fr>
2024-12-24 15:58:37 +0100

    Play with different example of high level observable operators (mergeMap, concatMap, exhaustMap, switchMap)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
-import { filter, interval, map, Observable, tap } from 'rxjs';
+import { delay, filter, interval, map, mergeMap, Observable, of, take, tap } from 'rxjs';
 import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    AsyncPipe,
-    HeaderComponent,
-    RouterOutlet
+    // AsyncPipe,
+    // HeaderComponent,
+    // RouterOutlet
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit{
 
-  interval$!: Observable<string>;
+  redTrainsCalled = 0;
+  yellowTrainsCalled = 0;
 
-  ngOnInit(): void {
-    this.interval$ = interval(1000).pipe(
-      filter(value => value % 3 === 0),
-      map(value => value % 2 === 0 ?
-        `Je suis ${value} et je suis pair`:
-        `Je suis ${value} et je suis impair`
-      ),
-      tap(text => this.logger(text))
+  ngOnInit() {
+    interval(500).pipe(
+      take(10),
+      map(value => value % 2 === 0 ? 'rouge' : 'jaune'),
+      tap(color => console.log(`La lumière s'allume en %c${color}`, `color: ${this.translateColor(color)}`)),
+      mergeMap(color => this.getTrainObservable$(color)),
+      tap(train => console.log(`Train %c${train.color} ${train.trainIndex} arrivé !`, `font-weight: bold; color: ${this.translateColor(train.color)}`))
+    ).subscribe();
+  }
+
+  getTrainObservable$(color: 'rouge' | 'jaune') {
+    const isRedTrain = color === 'rouge';
+    isRedTrain ? this.redTrainsCalled++ : this.yellowTrainsCalled++;
+    const trainIndex = isRedTrain ? this.redTrainsCalled : this.yellowTrainsCalled;
+    console.log(`Train %c${color} ${trainIndex} appelé !`, `text-decoration: underline; color: ${this.translateColor(color)}`);
+    return of({ color, trainIndex }).pipe(
+      delay(isRedTrain ? 5000 : 6000)
     );
   }
 
-  logger(text: string) {
-    console.log(`Log: ${text}`);
+  translateColor(color: 'rouge' | 'jaune') {
+    return color === 'rouge' ? 'red' : 'yellow';
   }
 }
