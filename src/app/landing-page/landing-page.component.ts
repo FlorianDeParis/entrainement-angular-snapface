1217202ec8baa9be0ddc29db873c94275e673ec6
Florian Tournay <florian.tournay@hotmail.fr>
2024-12-26 17:37:29 +0100

    Template form | Add two-way binding with ngSubmit

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,19 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    FormsModule
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
+
+  userEmail: string = 'lorem.ipsum@sitename.com';
+
   constructor(private router: Router){}
+
+  ngOnInit(): void {}
 
   onContinue(){
     this.router.navigateByUrl('facesnaps');
+  }
+
+  onSubmitForm(): void{
+    console.log(this.userEmail);
   }
 }
