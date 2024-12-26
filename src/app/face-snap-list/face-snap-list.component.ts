ae41bd5bcdcb0a0716e014b04d11d315a77cc838
Florian Tournay <florian.tournay@hotmail.fr>
2024-12-26 15:47:45 +0100

    2nd (and most important) memory leakage prevention: combine subject special Observer with 'takeUntil()' operator and ngOnDestroy life cycle hook

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap';
 import { FaceSnapComponent } from '../face-snap/face-snap.component';
 import { FaceSnapsService } from './../services/face-snaps.service';
-import { interval, take, tap } from 'rxjs';
+import { interval, Subject, takeUntil, tap } from 'rxjs';
 
 @Component({
   selector: 'app-face-snap-list',
@@ -13,19 +13,23 @@ import { interval, take, tap } from 'rxjs';
   templateUrl: './face-snap-list.component.html',
   styleUrl: './face-snap-list.component.scss'
 })
-export class FaceSnapListComponent implements OnInit{
+export class FaceSnapListComponent implements OnInit, OnDestroy{
   faceSnaps!: FaceSnap[];
+  private destroy$!: Subject<boolean>;
 
-  constructor(private FaceSnapsService: FaceSnapsService){
-
-  }
+  constructor(private FaceSnapsService: FaceSnapsService){}
 
   ngOnInit(): void {
+    this.destroy$ = new Subject<boolean>();
     this.faceSnaps = this.FaceSnapsService.getFaceSnaps();
 
     interval(1000).pipe(
-      take(3),
+      takeUntil(this.destroy$),
       tap(value => console.log(value))
     ).subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next(true);
   }
 }
