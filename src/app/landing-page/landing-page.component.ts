7e19e06adf90a74116ccbc648297f5445df5240f
Florian Tournay <florian.tournay@hotmail.fr>
2024-12-29 14:17:40 +0100

    Clean landing page component code

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,30 +1,21 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-landing-page',
   standalone: true,
   imports: [
-    RouterLink,
-    FormsModule
+    RouterLink
   ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
 export class LandingPageComponent implements OnInit {
-  userName: string = '';
-  userEmail: string = 'lorem.ipsum@sitename.com';
-
   constructor(private router: Router){}
 
   ngOnInit(): void {}
 
   onContinue(){
     this.router.navigateByUrl('facesnaps');
-  }
-
-  onSubmitForm(form: NgForm): void{
-    console.log(form.value);
   }
 }
