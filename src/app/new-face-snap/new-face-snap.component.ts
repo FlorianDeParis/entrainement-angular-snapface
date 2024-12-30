78896f3a19a662ae21bd724a2ffbcd639781fa7b
Florian Tournay <florian.tournay@hotmail.fr>
2024-12-30 12:10:43 +0100

    Add new facesnap route / that route will be used for a reactive form

---
+++ src/app/new-face-snap/new-face-snap.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-new-face-snap',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule
+  ],
+  templateUrl: './new-face-snap.component.html',
+  styleUrl: './new-face-snap.component.scss'
+})
+export class NewFaceSnapComponent {
+
+}
--- src/app/routes-list/routes-list.component.ts
+++ src/app/routes-list/routes-list.component.ts
@@ -26,6 +26,7 @@ export class RoutesListComponent implements OnInit {
       { id:"start", name:"Accueil", path:"" },
       { id:"facesnaplist", name:"Liste des FaceSnaps", path:"/facesnaps" },
       { id:"facesnapcard", name:"Accès à la carte d'un FaceSnap", path:`/facesnaps/${this.randomSnapId}`, desc: "Id du FaceSnap requis pour y accéder"},
+      { id:"facesnapcreate", name:"Créer un FaceSnap", path:'/create'},
       { id:"templateform", name:"Exemple d'un 'template form'", path:"/template-form" },
       { id:"routeslist", name:"Index des routes disponibles", path:"/routes-list" },
     ];
