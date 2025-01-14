975e9f6e880a87069486807e59eaeac61caf8b6a
Florian Tournay <florian.tournay@hotmail.fr>
2025-01-14 21:12:56 +0100

    Use subscription to wrap observables and use it to unsub when there is a ngOnDesctruct

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FaceSnap } from '../models/face-snap';
 import { FaceSnapComponent } from '../face-snap/face-snap.component';
 import { FaceSnapsService } from './../services/face-snaps.service';
-import { interval, Observable, Subject, takeUntil, tap } from 'rxjs';
+import { interval, Observable, Subject, Subscription, takeUntil, tap } from 'rxjs';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -18,23 +18,27 @@ import { CommonModule } from '@angular/common';
 export class FaceSnapListComponent implements OnInit, OnDestroy{
   // faceSnaps!: FaceSnap[];
   facesnaps$!: Observable<FaceSnap[]>;
-  private destroy$!: Subject<boolean>;
+  private destroy$: Subject<void> = new Subject();
+  private subscription: Subscription = new Subscription();
 
   constructor(private FaceSnapsService: FaceSnapsService){}
 
   ngOnInit(): void {
     // this.faceSnaps = this.FaceSnapsService.getFaceSnaps();
 
-    // this.destroy$ = new Subject<boolean>();
-    // interval(1000).pipe(
-    //   takeUntil(this.destroy$),
-    //   tap(value => console.log(value))
-    // ).subscribe();
+    this.subscription.add(
+      interval(1000).pipe(
+        // takeUntil(this.destroy$),
+        tap(value => console.log(value))
+      ).subscribe()
+    );
 
     this.facesnaps$ = this.FaceSnapsService.getFaceSnaps();
   }
 
   ngOnDestroy(): void {
-    // this.destroy$.next(true);
+    // this.destroy$.next();
+    // this.destroy$.complete();
+    this.subscription.unsubscribe();
   }
 }
