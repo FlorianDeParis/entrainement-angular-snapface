350c82121e6d754d3a5b737a002dc538bc1b7aa5
Florian <florian.tournay@hotmail.fr>
2025-01-15 18:03:55 +0100

    WTF ??? It doesn't display refreshed data...

--- src/app/routes-list/routes-list.component.ts
+++ src/app/routes-list/routes-list.component.ts
@@ -3,12 +3,14 @@ import { customRoute } from '../models/routes-list.type';
 import { FaceSnapsService } from './../services/face-snaps.service';
 import { RouterLink } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
-import { from, map, Observable, of, takeUntil, tap } from 'rxjs';
+import { combineLatest, concat, concatMap, from, map, Observable, of, takeUntil, tap, zip } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-routes-list',
   standalone: true,
   imports: [
+    CommonModule,
     RouterLink
   ],
   templateUrl: './routes-list.component.html',
@@ -16,61 +18,44 @@ import { from, map, Observable, of, takeUntil, tap } from 'rxjs';
 })
 
 export class RoutesListComponent implements OnInit {
-  myRoutes!: customRoute[];
-  FaceSnap$!: Observable<FaceSnap[]>;
+  faceSnap$!: Observable<FaceSnap[]>;
   randomSnap$!: Observable<FaceSnap>;
-  returnedFaceSnap!: FaceSnap;
+
+  myRoutes!: customRoute[];
+  routes$!: Observable<customRoute[]>;
 
   constructor(private FaceSnapsService: FaceSnapsService){
-    // this.randomSnapId = this.FaceSnapsService.getRandomFaceSnap().id.toString();
     this.randomSnap$ = this.FaceSnapsService.getRandomFaceSnap$();
-    console.log('random$');
-    this.FaceSnap$ = this.FaceSnapsService.getFaceSnaps();
+    this.faceSnap$ = this.FaceSnapsService.getFaceSnaps();
   }
 
   ngOnInit(): void {
-    const routes: Observable<customRoute> = from(this.myRoutes);
     this.myRoutes = [
       { id:"start", name:"Accueil", path:"" },
       { id:"facesnaplist", name:"Liste des FaceSnaps", path:"/facesnaps" },
-      // { id:"facesnapcard", name:"Accès à la carte d'un FaceSnap", path:`/facesnaps/${this.randomSnapId}`, desc: "Id du FaceSnap requis pour y accéder"},
       { id:"facesnapcard", name:"Accès à la carte d'un FaceSnap", path:`/facesnaps/__randomFaceSnapId__`, desc: "Id du FaceSnap requis pour y accéder"},
       { id:"facesnapcreate", name:"Créer un FaceSnap", path:'/create'},
       { id:"templateform", name:"Exemple d'un 'template form'", path:"/template-form" },
       { id:"routeslist", name:"Index des routes disponibles", path:"/routes-list" },
     ];
+    this.routes$ = of(this.myRoutes);
 
-
-    this.randomSnap$.pipe(
-      tap(data => {
-        console.log(data);
-      })
-    ).subscribe(data => {
-      this.returnedFaceSnap = data;
-      routes.pipe(
-        takeUntil(this.randomSnap$),
-        tap(route => console.log(route)),
-        map(
-          route => ({
-            ...route,
-            path : route.path.replace("__randomFaceSnapId__",data.id)
+    this.randomSnap$.subscribe(data => {
+      console.log(data);
+      this.routes$.pipe(
+        map(routes => {
+          return routes.map(route => {
+            return {
+              ...route,
+              path : route.path.replace("__randomFaceSnapId__",data.id)
+            };
           })
-        ),
-        tap(route => console.log('custom route: '+ route))
+        }),
+        tap(routes => {
+          console.log('custom route: ');
+          console.log(routes);
+        })
       ).subscribe();
     })
-
-
-    // routes.pipe(
-    //   takeUntil(this.randomSnap$),
-    //   tap(route => console.log(route)),
-    //   map(
-    //     route => ({
-    //       ...route,
-    //       path : route.path.replace("__randomFaceSnapId__",this.returnedFaceSnap.id)
-    //     })
-    //   ),
-    //   tap(route => console.log('custom route: '+ route))
-    // ).subscribe();
   }
 }
